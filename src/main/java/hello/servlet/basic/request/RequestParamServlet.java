ce08440564c8f19f6006a553de4158cd6d395dfb
dschoi <plainds@gmail.com>
2021-03-22 02:39:14 +0900

    first commit

---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,36 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+/**
+ * 1. 파라미터 전송 기능
+ * http://localhost:8080/request-param?username=hello&age=20
+ */
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("[전체 파라미터 조회] - start");
+        
+        req.getHeaderNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println(paramName + " = " + req.getParameter(paramName)));
+
+        System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
+
+        String username = req.getParameter("username");
+        String age = req.getParameter("age");
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+        System.out.println();
+
+        resp.getWriter().write("OK");
+    }
+}
